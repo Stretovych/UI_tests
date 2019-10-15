6c44ef6ae2d4c3c0f9a520ebef5a2e2944186b2e
stretovyych <box.antona@gmail.com>
2019-10-15 15:45:42 +0300

    Initial commit

---
+++ src/test/java/UI_tests/Balance_calculation.java
@@ -0,0 +1,14 @@
+package UI_tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Balance_calculation {
+    @Test
+    void Login(){
+        open("https://status.dev03.falconcrm.io/login");
+    }
+
+
+}
